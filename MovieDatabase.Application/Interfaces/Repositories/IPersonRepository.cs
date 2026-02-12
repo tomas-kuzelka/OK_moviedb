3c73e13394ea8ef9e3a0b3763f7b64955c945320
tomaskuzelka <tkuzelka@gmail.com>
2026-02-12 11:13:13 +0100

    add GetAll methods in PersonRepository

--- MovieDatabase.Application/Interfaces/Repositories/IPersonRepository.cs
+++ MovieDatabase.Application/Interfaces/Repositories/IPersonRepository.cs
@@ -7,5 +7,10 @@ namespace MovieDatabase.Application.Interfaces.Repositories;
 
 public interface IPersonRepository : IBaseRepository<Person, int>
 {
+    Task<Person?> GetWithMoviesAsync(int id, CancellationToken td = default);
 
+
+    Task<IEnumerable<Person>> GetAllWithMoviesAsync(int pageNumber = 1, int pageSize = 10, CancellationToken ct = default);
+
+    Task<IEnumerable<Person>> GetAllPersonsAsync(PersonRole personRole, int pageNumbe = 1, int pageSize = 10, CancellationToken ct = default);
 }
--- MovieDatabase.Infrastructure/Repositories/PersonRepository.cs
+++ MovieDatabase.Infrastructure/Repositories/PersonRepository.cs
@@ -13,8 +13,33 @@ public class PersonRepository : BaseRepository<Person, int>, IPersonRepository
     public PersonRepository(MoviesDbContext context) : base(context)
     {
     }
+    public async Task<Person?> GetWithMoviesAsync(int id, CancellationToken td = default)
+    {
+        return await _dbSet
+            .Include(p => p.MoviesAsDirector)
+            .Include(p => p.MoviesAsActor)
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
 
+    public async Task<IEnumerable<Person>> GetAllWithMoviesAsync(int pageNumber = 1, int pageSize = 10, CancellationToken ct = default)
+    {
+        return await _dbSet.AsNoTracking()
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .Include(p => p.MoviesAsDirector)
+            .Include(p => p.MoviesAsActor)
+            .ToListAsync(ct);
+    }
 
+    public async Task<IEnumerable<Person>> GetAllPersonsAsync(PersonRole personRole, int pageNumbe = 1, int pageSize = 10, CancellationToken ct = default)
+    {
+        return await _dbSet.AsNoTracking()
+            .Where(p => p.Role == personRole)
+            .OrderBy(p => p.Name)
+            .Skip((pageNumbe - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(ct);
+    }
 
 }
 
